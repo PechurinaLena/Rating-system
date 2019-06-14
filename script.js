fb86f7e1819b3341a623b1c52ccd425c0576ac0f
Elena Pechurina <www.waaay.net@gmail.com>
2019-06-14 13:40:48 +0300

    fix: add star content and change script.js

--- script.js
+++ script.js
@@ -1,60 +1,12 @@
-var rating = document.querySelector('.rating'),
-    ratingItem = document.querySelectorAll('.rating-item');
-
-rating.onclick = function (e) {
-    var target = e.target;
-    if (target.classList.contains('rating-item')) {
-        removeClass(ratingItem, 'current-active');
-        target.classList.add('active', 'current-active');
-    }
-};
-
-rating.onmouseover = function (e) {
-    var target = e.target;
-    if (target.classList.contains('rating-item')) {
-        removeClass(ratingItem, 'active');
-        target.classList.add('active');
-        mouseOverActiveClass(ratingItem);
-    }
-};
-
-rating.onmouseout = function () {
-    addClass(ratingItem, 'active');
-    mouseOutActiveClass(ratingItem);
-};
-
-function removeClass(arr) {
-    for (var i = 0, iLen = arr.length; i < iLen; i++) {
-        for (var j = 1; j < arguments.length; j++) {
-            ratingItem[i].classList.remove(arguments[j]);
+(function () {
+    var container = document.querySelector('.rating');
+    var items = container.querySelectorAll('.rating-item');
+    container.onclick = function (e) {
+        if (!e.target.classList.contains('active')) {
+            items.forEach(function (item) {
+                item.classList.remove('active');
+            });
+            e.target.classList.add('active');
         }
-    }
-}
-
-function addClass(arr) {
-    for (var i = 0, iLen = arr.length; i < iLen; i++) {
-        for (var j = 1; j < arguments.length; j++) {
-            ratingItem[i].classList.add(arguments[j]);
-        }
-    }
-}
-
-function mouseOverActiveClass(arr) {
-    for (var i = 0, iLen = arr.length; i < iLen; i++) {
-        if (arr[i].classList.contains('active')) {
-            break;
-        } else {
-            arr[i].classList.add('active');
-        }
-    }
-}
-
-function mouseOutActiveClass(arr) {
-    for (var i = arr.length - 1; i >= 1; i--) {
-        if (arr[i].classList.contains('current-active')) {
-            break;
-        } else {
-            arr[i].classList.remove('active');
-        }
-    }
-}
+    };
+})();
