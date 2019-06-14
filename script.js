f22d83a7638fadbec4e8b61e806023c2c1140ab1
Elena Pechurina <www.waaay.net@gmail.com>
2019-06-14 15:08:29 +0300

    fix: change script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-(function () {
-    var container = document.querySelector('.rating');
-    var items = container.querySelectorAll('.rating-item');
-    container.onclick = function (e) {
-        if (!e.target.classList.contains('active')) {
-            items.forEach(function (item) {
-                item.classList.remove('active');
-            });
-            e.target.classList.add('active');
-        }
-    };
-})();
+ document.documentElement.onclick = function (el) {
+     if (el.target.tagName !== 'DIV') return false;
+     el.target.classList.toggle('active');
+ };
